d4028ee2588965fa29f5f31122daa5d6e42f541d
Kauperwood <noblethrow@gmail.com>
2017-09-15 01:59:51 +0100

    added discount rules for whole order & for cheapest product

--- application/rules.py
+++ application/rules.py
@@ -1,3 +1,7 @@
+# This helper class for testing if customer has a discount, calculating if success
+# Description:
+# A customer who has already bought for over 1000 euro, 
+# gets a discount of 10% on the whole order.
 class RuleOne:
 	##
 	# Test method
@@ -12,7 +16,10 @@ class RuleOne:
 	# @param customer Customer instance
 	# @return array
 	def calc(self, order, customer):
-		return 'case1'
+		return {
+			'discount': (float(order.getTotal()) / float(100)) * 10,
+			'description': "A customer who has already bought for over 1000 euro, gets a discount of 10% on the whole order."
+		}
 # This helper class for testing if customer has a discount, calculating if success
 # Description:
 # For every products of category 'Switches' (id 2), 
@@ -43,11 +50,20 @@ class RuleThree:
 	# @param customer Customer instance
 	# @return bool
 	def test(self, order, customer):
-		return False
+		quantity = 0
+		for item in order.getItems():
+			if int(item.getCategory()) is int(1):
+				quantity += int(item.getQuantity())
+		return quantity >= 2
 	##
 	# Calculate method
 	# @param order Order instance
 	# @param customer Customer instance
 	# @return array
 	def calc(self, order, customer):
-		return 'case3'
+		minimum = min([float(v.getPrice()) for v in order.getItems() if int(v.getCategory()) is int(1)]) 
+		return {
+			'discount': (float(minimum) / float(100)) * 20,
+			'description': "If you buy two or more products of category 'Tools' (id 1), ou get a 20% discount on the cheapest product."
+		} 
+
